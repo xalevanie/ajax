94a82e161103a09ef616c5fd49384ff6c852aa9e
Madeline <mbrine@vcd-ceb131-01.eastern.ewu.edu>
2017-05-04 16:44:12 -0700

    air table

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,33 @@
 // @codekit-prepend "jquery.js";
 // @codekit-prepend "semantic.js";
+// @codekit-prepend "airtable.js";
 
 // 'load()' method
 // $('#data').load('/data/data.txt');
 // $('#data').load('/data/data.txt');
-$('#data').load('/data/data.json');
+//$('#data').load('/data/data.json');
+
+// Get from Airtable, Configuring
+
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: 'YOUR_API_KEY'}).base('appzkBUcTfGMlk7zW');
+
+base('Entertainers').select({
+    // Selecting the first 3 records in Grid view:
+    maxRecords: 3,
+    view: "Grid view"
+}).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('Name'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+}, function done(err) {
+    if (err) { console.error(err); return; }
+});
